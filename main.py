49501f324ebcde16bac119e6f1aa1d953b658234
AZS <arafezawadsajid16@gmail.com>
2024-06-14 16:48:44 +0600

    upto 1:13:13

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#run: "uvicorn main:app" 
-#append: "--reload"
+#run: "uvicorn main:app" and append: "--reload"
 
 from fastapi import FastAPI
 from fastapi.params import Body
@@ -7,27 +6,47 @@ from pydantic import BaseModel
 
 app = FastAPI()  #fastapi instance
 
-# pydantic model/schema
+#pydantic model/schema
 class Post(BaseModel):
-    title: str
-    content: str
+    title: str  #var name and data type same as body
+    content: str  #var name and data type same as body
 
 
-# path operation/route
+#path operation/route
 @app.get("/")  #decorator
 async def root():  #function
-    return {"message": "welcome to my api"}
+    return {"message": "welcome to my api"}  #sends this to the Get request
+
+
+#it will only run the first path operation that matches
+# @app.get("/")
+# async def get_posts():
+#     return {"data": "this is your posts"}
+
+#the above get() and this post() are not the same thing
+# @app.post("/")  #decorator
+# async def root():  #function
+#     return {"message2": "welcome to my api"}
+
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "your posts"}
+    return {"data": "this is your posts"}
+
+#hits the first one irrespective of method name
+# def root():  #function
+#     return {"message": "welcome to my api"}
+
 
 @app.post("/createposts")
-# def create_posts(payload: dict = Body(...)):
+# def create_posts(payload: dict = Body(...)):  #extracting payload
 #     print(payload)
 #     return {"new_post": f"title {payload['title']} content {payload['content']}"}
 def create_posts(new_post: Post):
-    print(new_post)
-    return {"data": "new post"}
+    print(new_post)  #new_post receives some JSON data from the Body
+    return {"data": "new post"}  #sends this to the Post request
 
-# check video and add timestamp
+
+
+
+#timestamp: https://youtu.be/0sOvCWFmrtA?si=ih3pZheDmErUAD9k&t=4393
