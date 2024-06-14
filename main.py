af9c2c6a1711f98507034bf07899fb622a6ca3b1
AZS <arafezawadsajid16@gmail.com>
2024-06-14 23:48:12 +0600

    upto 2:01:50

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #run: "uvicorn main:app" and append: "--reload"
 
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -70,21 +70,50 @@ def get_posts():
 #     print(payload)
 #     return {"new_post": f"title {payload['title']} content {payload['content']}"}
 
-@app.post("/posts")
-def create_posts(new_post: Post):
-    # print(new_post)  #new_post receives some JSON data from the Body
+# @app.post("/posts")
+# def create_posts(new_post: Post):
+    # print(new_post)  #new_post receives some JSON data from the Post request Body
     # print(new_post.published)
-    post_dict = new_post.dict()
-    post_dict['id'] = randrange(0, 1000000)
+    # return {"data": "new post"}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(new_post: Post):
+    post_dict = new_post.dict()  #new_post receives some JSON data from the Post request Body
+    post_dict['id'] = randrange(0, 1000000)  #adds a random id to post_dict
+    # print(new_post)
+    # print(post_dict)
     my_posts.append(post_dict)
     return {"data": post_dict}  #sends this to the Post request
 
 
-@app.get("/posts/{id}")  #this "id" field is a path parameter
-def get_post(id: int):  #data type validation
-    # print(id)  #path params are always str
+# @app.get("/posts/{id}")  #this "id" field is a path parameter, path params are always str
+# def get_post(id: int):  #data type validation
+    # print(id)
     # return {"post_detail": f"here is post {id}"}
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        # response.status_code = 404  #write status code 
+        # response.status_code = status.HTTP_404_NOT_FOUND  #use enum to find status code
+        # return {"message": f"post with id: {id} was not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
     return {"post_detail": post}
 
-#timestamp: https://youtu.be/0sOvCWFmrtA?si=i4j4wDuCLpYgIvx4&t=6480
+#when we try to visit "/posts/latest" but "/posts/{id}" is run, route matched by accident
+#due to such cases the order matters, be careful when using path params
+#change URL or rearrange accordingly to avoid such cases
+# @app.get("/posts/latest")
+# def get_latest_post():
+#     post = my_posts[len(my_posts)-1]
+#     return {"detail": post}
+
+
+
+
+
+
+
+#timestamp: https://youtu.be/0sOvCWFmrtA?si=iFn4DMQ1ejvBnQgf&t=7310
