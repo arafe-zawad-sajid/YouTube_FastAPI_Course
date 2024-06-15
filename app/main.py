c9d144dcae039d6eeec4f80e0dcecbf1894e1bbc
AZS <arafezawadsajid16@gmail.com>
2024-06-16 02:35:16 +0600

    upto db install

--- app/main.py
+++ app/main.py
@@ -152,4 +152,4 @@ def update_post(id: int, post: Post):  #validate the data from frontend that is 
 
 
 
-#timestamp: https://youtu.be/0sOvCWFmrtA?si=GKaTt_O5GnXdvX6F&t=8659
+#timestamp: https://youtu.be/0sOvCWFmrtA?si=8lBz45wvEO_K_Hv1&t=9266
