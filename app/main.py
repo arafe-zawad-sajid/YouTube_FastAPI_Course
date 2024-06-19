3fe7dad1643c7240a764982ae96f9d15bc6c481e
AZS <arafezawadsajid16@gmail.com>
2024-06-19 17:50:18 +0600

    upto upto 5:29:49

--- app/main.py
+++ app/main.py
@@ -25,6 +25,22 @@ class Post(BaseModel):
     title: str  #mandatory property
     content: str  #mandatorty property
     published: bool = True  #optional property
+    # rating: Optional[int] = None  #fully optional field
+
+
+#--- Connecting to Database ---#
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', 
+#                                 user='postgres', password='admin', cursor_factory=RealDictCursor)
+        
+#         cursor = conn.cursor()
+#         print('DB conn was successful')
+#         break
+#     except Exception as error:
+#         print('DB conn failed')
+#         print('Error:', error)
+#         time.sleep(2)
 
 
 #path operation/route
@@ -45,6 +61,9 @@ async def root():  #function
 #--- Get Posts---#
 @app.get("/posts")
 def get_posts(db: Session=Depends(get_db)):
+    # cursor.execute(""" SELECT * FROM posts """)
+    # posts = cursor.fetchall()  #retrieve multiple posts
+
     posts = db.query(models.Post).all()
     return {"data": posts}
 
@@ -53,15 +72,29 @@ def get_posts(db: Session=Depends(get_db)):
 #sending status code with the decorator
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db: Session=Depends(get_db)):
+    # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()  #save finalized staged changes
+
+    # new_post = models.Post(  #create a new post
+    #     title=post.title, content=post.content, published=post.published)
     
+    new_post = models.Post(**post.dict())  #auto unpack all fields of pydantic model
+    db.add(new_post)  #add it to db
+    db.commit()  #commit it
+    db.refresh(new_post)  #retrieve the new post we created in db
     return {"data": new_post}  #sends this to the Post request
 
 
 #--- Get A Post ---#
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id), ))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session=Depends(get_db)):
+    # cursor.execute(""" SELECT * FROM posts WHERE id=%s """, (str(id), ))
+    # post = cursor.fetchone()
+    
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # print(post)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
@@ -71,36 +104,44 @@ def get_post(id: int):
 #--- Delete A Post ---#
 #sending status code with the decorator
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute(""" DELETE FROM posts WHERE id=%s RETURNING * """, 
-                   (str(id), ))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post == None:
+def delete_post(id: int, db: Session=Depends(get_db)):
+    # cursor.execute(""" DELETE FROM posts WHERE id=%s RETURNING * """, 
+    #                (str(id), ))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    # print(type(post))  #<class 'sqlalchemy.orm.query.Query'>
+    if post_query.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
+    post_query.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 #--- Update A Post ---# #fix it#
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):  #validate the data from frontend that is stored in post with our Post schema
-    cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING * """,
-                   (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post == None:
+def update_post(id: int, post: Post, db: Session=Depends(get_db)):  #validate the data from frontend that is stored in post with our Post schema
+    # cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING * """,
+    #                (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    if post_query.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-    return {"data": updated_post}
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()}
+
+
+#schema/pydantic model vs. orm/sql alchemy model
+#main.Post class extends BaseModel which is imported from pydantic lib, this is our schema
+#it's being referenced in our path operations, it defines the shape of our request
+#models.Post class is our sql alchemy model, it defines what our db table looks like
 
 
 
-
-
-
-
-
-
-
-#upto upto 4:44:10 - https://youtu.be/0sOvCWFmrtA?si=1oC2iX2LGr8eMOA9&t=17053
+#upto upto 5:29:49 - https://youtu.be/0sOvCWFmrtA?si=km0SpcsUgCxShFEF&t=19789
